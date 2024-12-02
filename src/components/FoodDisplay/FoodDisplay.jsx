aed1910a44a72b561f132db739cf1463b9e04f4e
suryansh0010 <suryanshawasthi9@gmail.com>
2024-12-02 23:32:59 +0530

    category

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -12,8 +12,9 @@ const FoodDisplay = ({ category }) => {
             <h2>Top Dishes Near You</h2>
             <div className='food-display-list'>
                 {food_list && food_list.length > 0 ? (
-                    food_list.map((item) => (
-                        <FoodItem 
+                    food_list.map((item) => {
+                        if(category==="All" || category===item.category){
+                            return <FoodItem 
                             key={item._id}
                             id={item._id}
                             name={item.name}
@@ -21,7 +22,9 @@ const FoodDisplay = ({ category }) => {
                             price={item.price}
                             image={item.image}
                         />
-                    ))
+                        }
+                        
+                })
                 ) : (
                     <p>No food items available.</p>
                 )}
