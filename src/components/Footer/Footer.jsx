c236e83e92b8cd786d61203c268daf739aaedfb9
suryansh0010 <suryanshawasthi9@gmail.com>
2024-12-06 00:48:42 +0530

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import './Footer.css'
+import { assets } from '../../assets/assets'
 
 const Footer = () => {
   return (
     <div className='footer' id='footer'>
       <div className='footer-content'>
           <div className='footer-content-left'>
-
+              <img src={assets.logo} alt=''/>
+              <p>hi this is food website created by suryansh awasthi</p>
+              <div className='footer-social-icons'>
+                <img src={assets.facebook_icon} alt=''/>
+                <img src={assets.twitter_icon} alt=''/>
+                <img src={assets.linkedin_icon} alt=''/>
+              </div>
           </div>
           <div className='footer-content-center'>
-
+                
           </div>
           <div className='footer-content-right'>
 
@@ -18,6 +25,6 @@ const Footer = () => {
       </div>
     </div>
   )
-}
+} 
 
 export default Footer
