57714a7049b3f283f8610ed04dffb824ac017453
suryansh0010 <suryanshawasthi9@gmail.com>
2024-12-16 21:49:17 +0530

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import { assets } from '../../assets/assets'
 const Footer = () => {
   return (
     <div className='footer' id='footer'>
-      <div className='footer-content'>
+      <div className='footer-content' >
           <div className='footer-content-left'> 
               <img src={assets.logo} alt=''/>
               <p>hi this is food website created by suryansh awasthi</p>
