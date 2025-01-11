d02b23e4aab1d7cf85b342633e5cf91fc496a903
suryansh0010 <suryanshawasthi9@gmail.com>
2025-01-11 23:53:41 +0530

    update

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -9,7 +9,7 @@ const ExploreMenu = ({category ,setCategory}) => {
       </p>
       <div className='explore-menu-list'>
         {menu_list.map((item,index)=>{
-            return (
+            return ( 
                 <div onClick={()=>setCategory(prev => prev=== item.menu_name ? "All":item.menu_name)} key={index} className='explore-menu-list-item'>
                     <img className={category === item.menu_name?"active":""} src={item.menu_image} alt=''/>
                     <p>{item.menu_name}</p>
