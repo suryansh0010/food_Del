9d63a1bb64075b960c2dd32181ca1f9efcd122bd
suryansh0010 <suryanshawasthi9@gmail.com>
2025-01-12 14:32:27 +0530

    update

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -7,7 +7,7 @@ const ExploreMenu = ({category ,setCategory}) => {
       <h1>Explore Our Menu</h1>
       <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meel at a time
       </p>
-      <div className='explore-menu-list'>
+      <div className='explore-menu-list'> 
         {menu_list.map((item,index)=>{
             return ( 
                 <div onClick={()=>setCategory(prev => prev=== item.menu_name ? "All":item.menu_name)} key={index} className='explore-menu-list-item'>
