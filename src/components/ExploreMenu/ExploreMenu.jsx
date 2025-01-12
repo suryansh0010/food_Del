a6faf288c585c38d686f2b20a8a3a84528ad5630
suryansh0010 <suryanshawasthi9@gmail.com>
2025-01-12 14:33:41 +0530

    update

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -5,7 +5,7 @@ const ExploreMenu = ({category ,setCategory}) => {
   return (
     <div className='explore-menu' id='explore-menu'>
       <h1>Explore Our Menu</h1>
-      <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meel at a time
+      <p className='explore-menu-text'>Choose  from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meel at a time
       </p>
       <div className='explore-menu-list'> 
         {menu_list.map((item,index)=>{
