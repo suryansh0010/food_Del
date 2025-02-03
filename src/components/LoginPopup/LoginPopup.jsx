853e6256c435ea4a4022d532b818eea3cffd0e42
suryansh0010 <suryanshawasthi9@gmail.com>
2025-02-04 00:12:53 +0530

    updated

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -23,7 +23,9 @@ const LoginPopup = ({ setShowLogin }) => {
           <input type='checkbox' required/>
           <p>By continuing,i agree to term of use & privacy policy</p>
         </div>
-        {currState==="Login"?<p>Create a new account? <span>Click here</span></p>:<p>Already have an account? <span>Login here</span></p>}
+        {currState==="Login"?
+      <p>Create a new account<span onClick={()=>setCurrState("Sign Up")}>Click here</span></p>
+      :<p>Already have an account?<span onClick={()=>setCurrState("Login")}>Login here</span></p>}
         
         
       </form>
