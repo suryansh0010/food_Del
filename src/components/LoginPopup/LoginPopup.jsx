61c841ada86e0a76ee3efac6c11458191399970c
suryansh0010 <suryanshawasthi9@gmail.com>
2025-02-03 20:59:59 +0530

    update

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -1,20 +1,27 @@
-import React, { useState } from 'react'
-import './LoginPopup.css'
-import { assets } from '../../assets/assets'
-const LoginPopup = ({setShowLogin}) => {
+import React, { useState } from 'react';
+import './LoginPopup.css';
+import { assets } from '../../assets/assets';
 
-    const [currState,setCurrState] = useState("sign up")
+const LoginPopup = ({ setShowLogin }) => {
+
+  const [currState, setCurrState] = useState("sign up");
 
   return (
     <div className='login-popup'>
-      <from className='login-popup-container' >
+      <form className='login-popup-container'> {/* Change from <from> to <form> */}
         <div className='login-popup-title'>
           <h2>{currState}</h2>
-          <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt=''/>
+          <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt='' />
         </div>
-      </from>
+        <div className='login-popup-inputs'>
+          <input type='text' placeholder='Your name' required />
+          <input type='email' placeholder='Your email' required />
+          <input type='password' placeholder='Your password' required />
+        </div>
+        <button>{currState === "sign up" ? "Create account" : "Log In"}</button> 
+      </form>
     </div>
-  )
+  );
 }
 
-export default LoginPopup
+export default LoginPopup;
