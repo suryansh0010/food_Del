b357410778f8f11e7b077d91a591c18d0e6f87d2
suryansh0010 <suryanshawasthi9@gmail.com>
2025-02-04 22:43:27 +0530

    updated

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -6,7 +6,16 @@ const Cart = () => {
   return (
     <div className='cart'>
       <div className='cart-items'>
-
+          <div className='cart-items-title'>
+              <p>Items</p>
+              <p>Title</p>
+              <p>Price</p>
+              <p>Quantity</p>
+              <p>Total</p>
+              <p>Remove</p>
+          </div>
+          <br/>
+          <hr/>
       </div>
     </div>
   )
