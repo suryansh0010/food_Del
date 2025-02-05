a61611fb2fd864041f599b06e383f09295837b55
suryansh0010 <suryanshawasthi9@gmail.com>
2025-02-05 20:18:01 +0530

    updated

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -26,7 +26,7 @@ const Cart = () => {
                   <div className='cart-items-title cart-items-item'>
                        <img src={item.image} alt=''/>
                        <p>{item.name}</p>
-                       <p>{item.price}</p>
+                       <p>${item.price}</p>
                        <p>{cartItems[item._id]}</p>
                        <p>{item.price*cartItems[item._id]}</p>
                        <p onClick={()=>removeFromCart(item._id)} className='cross'>x</p>
