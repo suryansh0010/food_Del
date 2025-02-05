9bc841492706f741b50f1a657c41dfc0199f15d3
suryansh0010 <suryanshawasthi9@gmail.com>
2025-02-05 15:47:33 +0530

    updated

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,7 +1,8 @@
 import React, { useContext } from 'react'
 import './Cart.css'
+import { StoreContext } from '../../context/StoreContext';
 const Cart = () => {
-  const {cartItems,food_list,removeFromCart} = useContext(storeContext);
+  const {cartItems,food_list,removeFromCart} = useContext(StoreContext);
 
   return (
     <div className='cart'>
