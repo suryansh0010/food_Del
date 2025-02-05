abbf30cca8a7b8aad7c6265dfd6a020eed0cdeb7
suryansh0010 <suryanshawasthi9@gmail.com>
2025-02-05 15:47:16 +0530

    updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({setShowLogin}) => {
     const[menu,setmenu] = useState("home");
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt='' className='logo'/>
+      <Link to='/'><img src={assets.logo} alt='' className='logo'/></Link>
       <ul className='navbar-menu'>
         <Link to='/' onClick={()=>setmenu("home")} className={menu==="home"?"active":""}>home</Link>
         <a href='#explore-menu' onClick={()=>setmenu("menu")} className={menu==="menu"?"active":""}>menu</a>
@@ -16,7 +16,7 @@ const Navbar = ({setShowLogin}) => {
       <div className='navbar-right'> 
         <img src={assets.search_icon} alt=''/>
         <div className='navbar-search-icon'>
-            <img src={assets.basket_icon} alt=''/>
+            <Link to='/cart'><img src={assets.basket_icon} alt=''/></Link>
             <div className='dot'></div>
         </div>
         <button onClick={()=>setShowLogin(true)}>Sign in</button>
