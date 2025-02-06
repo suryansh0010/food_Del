61f8a8f1c74c46265b0c3eea833ef66b2321ecee
suryansh0010 <suryanshawasthi9@gmail.com>
2025-02-06 22:29:06 +0530

    updated

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import './PlaceOrder.css'
 const PlaceOrder = () => {
   return (
-    <div>
-      
-    </div>
+    <form className='place-order'>
+      <div className='place-order-left'>
+
+      </div>
+      <div className='place-order-right'>
+
+      </div>
+    </form>
   )
 }
 
