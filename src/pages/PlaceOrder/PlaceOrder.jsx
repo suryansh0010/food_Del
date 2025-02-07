b79801c38b49cabbe1e4caddd6194f4d37631d00
suryansh0010 <suryanshawasthi9@gmail.com>
2025-02-07 10:18:03 +0530

    updated

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import "./PlaceOrder.css";
+import { StoreContext } from "../../context/StoreContext";
 const PlaceOrder = () => {
 
   const {getTotalCartAmount} = useContext(StoreContext)
